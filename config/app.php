ac2b385cb423e14d59962963adccdb6868378bff
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2021-01-09 18:50:30 +0530

    understood config/app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'ndi'),
 
     /*
     |--------------------------------------------------------------------------
@@ -230,3 +230,9 @@ return [
     ],
 
 ];
+
+/**
+ * Reading: DONE
+ * Understanding: DONE
+ * 
+ */
